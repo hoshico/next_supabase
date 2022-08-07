25431bac18c406f9785a898f53e9bda81d34a7cf
hoshico <melancholinista.yasu@gmail.com>
2022-08-07 14:18:59 +0900

    add plyaers-data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,70 @@
-import type { GetServerSideProps, NextPage } from 'next'
+import type { GetServerSideProps, GetStaticProps, NextPage } from 'next'
 import { createClient } from '@supabase/supabase-js'
 import { useEffect, useState } from 'react'
 import { supabase } from '../libs/supabaseClient';
 import Image from 'next/image';
 
+
 type Player = {
+  avg: string;
+  hr: string;
   id: number;
+  image?: string;
   name: string;
-  result: string;
+  position: string;
 }
+avg: "300"
+hr: "30"
+id: 5
+image: null
+name: "ソト"
+position: "外野手"
+//type PlayerProps = NonNullable<Player[]>;
 
-type PlayerProps = NonNullable<Player[]>;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const { publicURL } = supabase.storage
-    .from('mlb-pic')
-    .getPublicUrl("betts.jpg");
+/*
+  最初にsupabaseから全てのデータを取得する
+*/
+export const getStaticProps: GetStaticProps = async () => {
+  let { data: player, error } = await supabase
+  .from('player')
+  .select('*')
     return {
       props: {
-        url: publicURL,
+        data: player,
       }
     }
 }
 
-const Home: NextPage = (props) => {
+const Home: NextPage<{data: Array<Player>}> = (props) => {
+  const { data } = props;
+
+  // 外野手データ
+  const outFieldPlayers = data.filter((player) => player.position === "外野手");
+  
+  // 三塁手
+  const thirdBasemanPlayers = data.filter((player) => player.position === "三塁手");
+  // 遊撃手
+  const shortStopPlayers = data.filter((player) => player.position === "遊撃手");
+  // 二塁手
+  const secondBasemanPlayers = data.filter((player) => player.position === "二塁手");
+  // 一塁手
+  const firstBasemanPlayers = data.filter((player) => player.position === "一塁手");
+
+  // 指名打者
+  const designatedHitterPlayers = data.filter((player) => player.position === "指名打者");
+
+
   // 外野手
-  const [ playerData, setPlayerData ] = useState<PlayerProps>([]);
+  const [ playerData, setPlayerData ] = useState<Player[]>([]);
   const [ selectPlayer, setSelectPlayer ] = useState<string>();
   const [ playerImgUrl, setPlayerImgUrl ] = useState<string>();
-  // 三塁手
-  const [ thirdPlayerData, setThirdPlayerData ] = useState<PlayerProps>([]);
-  const [ selectThirdPlayer, setSelectThirdPlayer ] = useState<string>();
-  // 遊撃手
-  const [ shortPlayerData, setShortPlayerData ] = useState<PlayerProps>([]);
-  const [ selectShortPlayer, setSelectShortPlayer ] = useState<string>();
-  // 二塁手
-  const [ secondPlayerData, setSecondPlayerData ] = useState<PlayerProps>([]);
-  const [ selectSecondPlayer, setSelectSecondPlayer ] = useState<string>();
-  
-  const handleChange = (e) => {
-    
-    setSelectPlayer(e.target.value);
-  }
-  const handleThirdChange = (e) => {
-    setSelectThirdPlayer(e.target.value);
-  }
-  const handleShortChange = (e) => {
-    setSelectShortPlayer(e.target.value);
-  }
-  const handleSecondChange = (e) => {
-    setSelectSecondPlayer(e.target.value);
-  }
-
+ 
+ 
   // 05/08追加 ドロップダウン
-  const selectPlayerList = (e) => {
-    console.log(e.target.dataset.url)
-    setPlayerImgUrl(e.target.dataset.url)
+  const selectPlayerList: React.MouseEventHandler<HTMLLIElement> = (e) => {
+    console.log(e.target)
+    //console.log(e.target.dataset.url)
+    //setPlayerImgUrl(e.target.dataset.url)
   };
   
  
@@ -64,17 +73,15 @@ const Home: NextPage = (props) => {
       // 外野手
       let { data: player }: { data:any } = await supabase.from('player').select().eq('position','外野')
       // 三塁手
-      let { data: thirdPlayer }: { data:any } = await supabase.from('player').select().eq('position','三塁')
+      //let { data: thirdPlayer }: { data:any } = await supabase.from('player').select().eq('position','三塁')
       // 遊撃手
-      let { data: shortPlayer }: { data:any } = await supabase.from('player').select().eq('position','遊撃')
+      //let { data: shortPlayer }: { data:any } = await supabase.from('player').select().eq('position','遊撃')
       // 二塁手
-      let { data: secondPlayer }: { data:any } = await supabase.from('player').select().eq('position','二塁')
+      //let { data: secondPlayer }: { data:any } = await supabase.from('player').select().eq('position','二塁')
       // 一塁手
-      let { data: firstPlayer }: { data:any } = await supabase.from('player').select().eq('position','一塁')
+      //let { data: firstPlayer }: { data:any } = await supabase.from('player').select().eq('position','一塁')
       setPlayerData(player)
-      setThirdPlayerData(thirdPlayer)
-      setShortPlayerData(shortPlayer)
-      setSecondPlayerData(secondPlayer)
+ 
     }
     getPlayData();
   },[])
@@ -83,19 +90,6 @@ const Home: NextPage = (props) => {
       <div className="bg-gray-200 min-h-screen">
         <div className="w-4/5 mx-auto pt-48 ">
           {/*外野手*/}
-          {/*<div className="outfield grid grid-cols-3 gap-20">
-            <div className="mt-14 w-full h-14 bg-black"></div>
-            <div className="w-full h-14 bg-black"></div>
-            <div className="mt-14 w-full h-14 bg-black"></div>
-          </div>*/}
-          {/*内野手*/}
-          {/*<div className="mt-8 w-full mx-auto outfield grid grid-cols-4 gap-10">
-            <div className="mt-14 w-full h-14 bg-black"></div>
-            <div className="w-full h-14 bg-black"></div>
-            <div className="w-full h-14 bg-black"></div>
-            <div className="mt-14 w-full h-14 bg-black"></div>
-          </div>*/}
-          {/*捕手*/}
           <div className="mt-8 w-1/5 mx-auto outfield grid grid-cols-1">
             <div className="mt-14 w-full">
               <div className="w-30 h-60 relative">
@@ -103,7 +97,6 @@ const Home: NextPage = (props) => {
                   <Image 
                   alt=''
                   src={playerImgUrl}
-                  //src="https://fysjbpwpllgjckgtvdqx.supabase.co/storage/v1/object/public/mlb-pic/bellinger.jpg"
                   layout='fill'
                   objectFit='cover'
                   className='block group-hover:opacity-75'
@@ -111,77 +104,19 @@ const Home: NextPage = (props) => {
               </div>
               <div className="dropdown">
                 <label tabIndex={0} className="btn m-1 px-6">外野手</label>
-                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 w-60">
+                {/*<ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 w-60">
                   {playerData.map((player) => (
                     <li className='mb-2 p-4 cursor-pointer hover:bg-white' key={player.id} data-url={player.imageSrc} onClick={selectPlayerList}>{player.name}</li>
                   ))}
-                </ul>
+                </ul>*/}
               </div>
-
-
-
               
               <div className="result mt-5">
                 <p>スタメン:{selectPlayer}</p>
               </div>
             </div>
           </div>
-          {/*DH*/}
-          {/*<div className="mt-8 w-4/5 mx-auto outfield grid grid-cols-1">
-            <div className="mt-14 ml-auto w-1/5 h-14 bg-black"></div>
-          </div>*/}
         </div>
-
-        {/*<div className="">
-          <label htmlFor="location" className="text-white block text-sm font-medium text-gray-700">
-             外野手
-          </label>
-          <select
-            id="location"
-            name="location"
-            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-            defaultValue="--"
-            onChange={(e) => handleChange(e)}
-          >
-            <option value="" className="text-black">選手を選択</option>
-            {playerData.map((player)=> (
-              <option value={player.name} className="text-black" key={player.id}>{player.name}/打率{player.result}</option>
-            ))}
-          </select>
-          <div className="result mt-5">
-            <p>スタメン:{selectPlayer}</p>
-          </div>
-        </div>*/}
-        {/*三塁手*/}
-        {/*<div className="ml-10">
-          <label htmlFor="location" className="text-white block text-sm font-medium text-gray-700">
-             三塁手
-          </label>
-          <select
-            id="location"
-            name="location"
-            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-            defaultValue="--"
-            onChange={(e) => handleThirdChange(e)}
-          >
-            <option value="" className="text-black">選手を選択</option>
-            {thirdPlayerData.map((thirdPlayer)=> (
-              <option value={thirdPlayer.name} className="text-black" key={thirdPlayer.id}>{thirdPlayer.name}/打率{thirdPlayer.result}</option>
-            ))}
-          </select>
-          <div className="result mt-5">
-            <p>スタメン:{selectThirdPlayer}</p>
-          </div>
-        </div>*/}
-
-        {/*<div className="ml-10">
-          <ul>
-            <li>外野手:{selectPlayer}</li>
-            <li>三塁手:{selectThirdPlayer}</li>
-            <li>遊撃手:{selectShortPlayer}</li>
-            <li>二塁手:{selectSecondPlayer}</li>
-          </ul>
-        </div>*/}
       </div>
     </>
   )
