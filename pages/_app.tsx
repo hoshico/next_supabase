19df781520c04563a329f2c5226089af32be494b
hoshico <melancholinista.yasu@gmail.com>
2022-10-09 17:37:03 +0900

    add userReducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
+export const selectUser = (state: any) => state.user.user;
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const Auth = () => {
+  const [login, setLogin] = useState(false);
   return (
     <>
       <section className="h-full gradient-form bg-gray-200 md:h-screen">
-        <div className="container py-12 px-6 h-full">
+        <div className="container py-12 px-6 h-full mx-auto">
           <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
             <div className="xl:w-10/12">
               <div className="block bg-white shadow-lg rounded-lg">
@@ -12,7 +15,7 @@ const Auth = () => {
                   <div className="lg:w-6/12 px-4 md:px-0">
                     <div className="md:p-12 md:mx-6">
                       <div className="text-center ">
-                        <h4 className="text-xl font-semibold mt-1 mb-12 pb-1">We are The Lotus Team</h4>
+                        <h4 className="text-xl font-semibold mt-1 mb-12 pb-1">ログインページ</h4>
                       </div>
                       {/*フォーム*/}
                       <form>
