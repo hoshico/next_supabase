7f03e35b114a92f1ae2033840cba18328ae81a5b
hoshico <melancholinista.yasu@gmail.com>
2022-10-10 17:43:35 +0900

    add login.tsx

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,6 +1,20 @@
-const Auth = () => {
+import { useForm, Controller } from "react-hook-form";
+import { supabase } from "../../libs/supabaseClient";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+const Signin = () => {
+  const { register, handleSubmit } = useForm();
+
+  // loginボタン
+  const onLogin = async(data: any) => {
+
+    console.log(data)
+    const res = await supabase.auth.signIn(data);
+  };
   return (
-  <>
     <section className="h-full gradient-form bg-gray-200 md:h-screen">
       <div className="container py-12 px-6 h-full mx-auto">
         <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
@@ -10,30 +24,32 @@ const Auth = () => {
                 <div className="lg:w-6/12 px-4 md:px-0">
                   <div className="md:p-12 md:mx-6">
                     <div className="text-center ">
-                      <h4 className="text-xl font-semibold mt-1 mb-12 pb-1">ログインページ</h4>
+                      <h4 className="text-xl font-semibold mt-1 mb-12 pb-1">ログイン</h4>
                     </div>
                     {/*フォーム*/}
-                    <form>
-                      <div className="mb-4">
+                    <form onSubmit={handleSubmit(onLogin)}>
+                      <div className="mb-4"> 
                         <input
+                          {...register('email')}
                           type="text"
                           className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                           id="exampleFormControlInput1"
-                          placeholder="Username"
+                          placeholder="email"
                         />
                       </div>
-                      <div className="mb-4">
+                      <div className="mb-4">  
                         <input
-                          type="password"
+                          {...register('password')}
+                          type="text"
                           className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                           id="exampleFormControlInput1"
-                          placeholder="Password"
+                          placeholder="password"
                         />
                       </div>
                       <div className="text-center pt-1 mb-12 pb-1">
                         <button
                           className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
-                          type="button"
+                          type="submit"
                           data-mdb-ripple="true"
                           data-mdb-ripple-color="light"
                           //style="
@@ -80,7 +96,7 @@ const Auth = () => {
         </div>
       </div>
     </section>
-  </>
-)
+  )
 };
-export default Auth;
+
+export default Signin;
