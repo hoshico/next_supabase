7a858649c3660e502dea238b86752541eff4a354
hoshico <melancholinista.yasu@gmail.com>
2023-02-01 22:50:32 +0900

    add select tab

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -1,9 +1,30 @@
 import { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
 
-interface Props {}
+interface Props {
 
-const index: NextPage<Props> = () => {
-  return <div>index</div>
+}
+
+const OrderPage: NextPage<Props> = () => {
+  const {register, handleSubmit, formState: {isDirty, dirtyFields}} = useForm({
+    defaultValues: {
+      firstPlayer: "",
+    }
+  })
+  const onSubmit = (data: any) =>{
+    console.log(data);
+  };
+  return (
+    <div className='bg-white'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <select>
+          <option>ddd</option>
+          <option>ddd</option>
+          <option>ddd</option>
+        </select>
+      </form>
+    </div>
+  )
 };
 
-export default index;
+export default OrderPage;
