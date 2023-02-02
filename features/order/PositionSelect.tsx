b8022bf48a0515dc54dda0638a20d06b047e5eb1
hoshico <melancholinista.yasu@gmail.com>
2023-02-02 21:59:41 +0900

    fix prettire

--- features/order/PositionSelect.tsx
+++ features/order/PositionSelect.tsx
@@ -1,13 +1,13 @@
 const PositionSelect = (props: any): JSX.Element => {
   const { players } = props;
-  console.log(players)
+  console.log(players);
   return (
     <select>
       {players.map((player: any) => (
         <option key={player.name}>{player.name}</option>
       ))}
     </select>
-  )
+  );
 };
 
 export default PositionSelect;
