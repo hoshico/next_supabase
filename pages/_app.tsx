e03121315a5d7fb002c451bcad4c48c94351adc7
hoshico <melancholinista.yasu@gmail.com>
2023-02-03 18:44:38 +0900

    apply recoilroot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'react-redux'
-import { store } from '../libs/store'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
-    <Provider store={store}>
+    <RecoilRoot>
       <Component {...pageProps} />
-    </Provider>
-  )
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default MyApp;
