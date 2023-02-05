efd82d961608da7177b649cd582e4958f35db0fc
hoshico <melancholinista.yasu@gmail.com>
2023-02-05 17:45:29 +0900

    add game logic#

--- features/game/index.tsx
+++ features/game/index.tsx
@@ -1,15 +1,66 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { teamState } from '../../states/atoms/inputAtom';
 
 const GameDetail = () => {
   const router = useRouter();
   const { userId, selectedTeam } = useRecoilValue(teamState);
+  const [score, setScore] = useState<(number | string)[]>([]);
+  const [result, setReasult] = useState('');
+
+  useEffect(() => {
+    let awayTotalScore =
+      (score[0] as number) +
+      (score[2] as number) +
+      (score[4] as number) +
+      (score[6] as number) +
+      (score[8] as number) +
+      (score[10] as number) +
+      (score[12] as number) +
+      (score[14] as number) +
+      (score[16] as number);
+
+    let homeTotalScore =
+      (score[1] as number) +
+      (score[3] as number) +
+      (score[5] as number) +
+      (score[7] as number) +
+      (score[9] as number) +
+      (score[11] as number) +
+      (score[13] as number) +
+      (score[15] as number);
+
+    if (score.length < 18) {
+      const timer = setTimeout(() => {
+        console.log('timer');
+        setScore([...score, 2]);
+      }, 100);
+
+      // 9回裏で負け確定
+      if (awayTotalScore < homeTotalScore) {
+        setScore([...score, 'X']);
+        return () => {
+          clearTimeout(timer);
+        };
+      }
+      // ９回裏まで
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+    if(score[17]){
+      setReasult("引き分け")
+    }
+  }, [score]);
+
   if (!selectedTeam) {
     return (
       <>
         <div>
-          <button type="button" onClick={() => router.push("team")}>チーム選択に戻る</button>
+          <button type='button' onClick={() => router.push('team')}>
+            チーム選択に戻る
+          </button>
         </div>
       </>
     );
@@ -18,54 +69,102 @@ const GameDetail = () => {
     <div className='pt-12'>
       {/*scroe board*/}
       <div className='m-auto w-3/5'>
-        <table>
+        <table className='w-full'>
           <caption>Scoreboard</caption>
           <thead>
-            <tr>
+            <tr className='border'>
               <th className=''>Team</th>
-              <th>1st</th>
-              <th>2nd</th>
-              <th>3rd</th>
-              <th>4th</th>
-              <th>5th</th>
-              <th>6th</th>
-              <th>7th</th>
-              <th>8th</th>
-              <th>9th</th>
+              <th className='border'>1st</th>
+              <th className='border'>2nd</th>
+              <th className='border'>3rd</th>
+              <th className='border'>4th</th>
+              <th className='border'>5th</th>
+              <th className='border'>6th</th>
+              <th className='border'>7th</th>
+              <th className='border'>8th</th>
+              <th className='border'>9th</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr className='border'>
               <td data-th='Team'>
                 <span className='long'>{selectedTeam}</span>
               </td>
-              <td data-th='1st'>0</td>
-              <td data-th='2nd'>1</td>
-              <td data-th='3rd'>0</td>
-              <td data-th='4th'>0</td>
-              <td data-th='5th'>1</td>
-              <td data-th='6th'>0</td>
-              <td data-th='7th'>0</td>
-              <td data-th='8th'>3</td>
-              <td data-th='9th'>0</td>
+              <td className='border' data-th='1st'>
+                {score[0]}
+              </td>
+              <td className='border' data-th='2nd'>
+                {score[2]}
+              </td>
+              <td className='border' data-th='3rd'>
+                {score[4]}
+              </td>
+              <td className='border' data-th='4th'>
+                {score[6]}
+              </td>
+              <td className='border' data-th='5th'>
+                {score[8]}
+              </td>
+              <td className='border' data-th='6th'>
+                {score[10]}
+              </td>
+              <td className='border' data-th='7th'>
+                {score[12]}
+              </td>
+              <td className='border' data-th='8th'>
+                {score[14]}
+              </td>
+              <td className='border' data-th='9th'>
+                {score[16]}
+              </td>
             </tr>
-            <tr>
+            <tr className='border'>
               <td data-th='Team'>
                 <span className='long'>Padres</span>
               </td>
-              <td data-th='1st'>0</td>
-              <td data-th='2nd'>3</td>
-              <td data-th='3rd'>0</td>
-              <td data-th='4th'>0</td>
-              <td data-th='5th'>0</td>
-              <td data-th='6th'>1</td>
-              <td data-th='7th'>0</td>
-              <td data-th='8th'>2</td>
-              <td data-th='9th'>0</td>
+              <td className='border' data-th='1st'>
+                {score[1]}
+              </td>
+              <td className='border' data-th='2nd'>
+                {score[3]}
+              </td>
+              <td className='border' data-th='3rd'>
+                {score[5]}
+              </td>
+              <td className='border' data-th='4th'>
+                {score[7]}
+              </td>
+              <td className='border' data-th='5th'>
+                {score[9]}
+              </td>
+              <td className='border' data-th='6th'>
+                {score[11]}
+              </td>
+              <td className='border' data-th='7th'>
+                {score[13]}
+              </td>
+              <td className='border' data-th='8th'>
+                {score[15]}
+              </td>
+              <td className='border' data-th='9th'>
+                {score[17]}
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
+      <div>{result}</div>
+      {/*試合開始*/}
+      {/*<div className='m-auto w-1/5 pt-6'>
+        <button
+          className='rounded-md bg-yellow-600 p-2 text-black'
+          onClick={playGame}
+        >
+          試合開始
+        </button>
+      </div>*/}
+      {/*結果*/}
+      <div></div>
     </div>
   );
 };
