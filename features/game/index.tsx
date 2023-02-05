94861270e44155338ff0c58def6ea43302fd29f8
hoshico <melancholinista.yasu@gmail.com>
2023-02-05 23:33:57 +0900

    apply score logic

--- features/game/index.tsx
+++ features/game/index.tsx
@@ -6,7 +6,7 @@ import { teamState } from '../../states/atoms/inputAtom';
 const GameDetail = () => {
   const router = useRouter();
   const { userId, selectedTeam } = useRecoilValue(teamState);
-  const [score, setScore] = useState<(number | string)[]>([]);
+  const [score, setScore] = useState<any[]>([]);
   const [result, setReasult] = useState('');
   const [gameResult, setGameResult] = useState<number[]>([]);
 
@@ -14,21 +14,45 @@ const GameDetail = () => {
     if (!selectedTeam) {
       router.push('/team');
     }
-    if (score.length < 18) {
-      const timer = setTimeout(() => {
-        setScore([...score, Math.floor(Math.random() * 3)]);
-      }, 100);
 
+    const timer = setTimeout(() => {
+      let awayScore = 0;
+      let homeScore = 0;
+
+      // 9回表で場合分け
+      if (score.length === 17) {
+        for (let i = 0; i < 17; i++) {
+          if (i % 2 === 0) {
+            awayScore += score[i];
+          } else {
+            homeScore += score[i];
+          }
+        }
+        if (awayScore < homeScore) {
+          setScore([...score, 'x']);
+          setReasult('負け');
+          setGameResult([awayScore, homeScore]);
+        } else {
+          const lastScore = Math.floor(Math.random() * 3);
+          setScore([...score, lastScore]);
+          awayScore === homeScore + lastScore
+            ? setReasult('引き分け')
+            : setReasult('勝ち');
+          setGameResult([awayScore, homeScore + lastScore]);
+        }
+      }
+      // 9回裏までの場合
+      if (score.length < 17) {
+        setScore([...score, Math.floor(Math.random() * 3)]);
+      }
+    }, 100);
+    if (score.length > 17) {
+      console.log('cleanInterval');
       return () => {
         clearInterval(timer);
       };
     }
-    const awayScore = result.reduce((cur, res) => {
-      
-    },[])
   }, [score]);
-
-  console.log(gameResult);
 
   return (
     <div className='pt-12'>
@@ -119,11 +143,11 @@ const GameDetail = () => {
         </table>
       </div>
 
-      <div>
-        {gameResult[0]}-{gameResult[1]}
+      <div className='pt-4 mx-auto w-3/5'>
+        {gameResult.length > 1 && <p className='text-3xl'>{`${gameResult[0]}-${gameResult[1]}`}</p>}
+        <p className='text-3xl'>{result}</p>
       </div>
 
-      <div>{result}</div>
       {/*試合開始*/}
       {/*<div className='m-auto w-1/5 pt-6'>
         <button
