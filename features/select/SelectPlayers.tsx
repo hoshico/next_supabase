8bfc517136cec92714844f5bc55d8e220de9488b
hoshico <melancholinista.yasu@gmail.com>
2023-06-18 13:04:23 +0900

    add api setting

--- features/select/SelectPlayers.tsx
+++ features/select/SelectPlayers.tsx
@@ -3,6 +3,15 @@ import useSWR from 'swr';
 export const SelectPlayers = () => {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data, error } = useSWR('../api/players', fetcher);
-  console.log("選手情報: ",data)
-  return <></>;
+
+  console.log('player情報: ', data);
+  if (error) return <div>Not Found</div>;
+  if (!data) return <div>loading...</div>;
+  return (
+    <div>
+      {data.map((player: any, index: number) => (
+        <p key={index}>{player.englishName}</p>
+      ))}
+    </div>
+  );
 };
--- pages/api/players.ts
+++ pages/api/players.ts
@@ -2,7 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { supabase } from '../../libs/supabaseClient';
 
 const plyaersApi = async (_: NextApiRequest, res: NextApiResponse) => {
-  let { data, error } = await supabase.from('players').select('*');
+  let { data, error } = await supabase
+    .from('players')
+    .select('*')
+
+    // 絞り込み方法
+    // .eq('position', 'SS');
 
   if (error) {
     return res.status(500).json({ message: 'Not Found' });
